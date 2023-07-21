a227b6968d346a67f733b3f47bf1340cb68f6288
ddarshit <darshitdayala007@gmail.com>
2023-07-21 23:22:48 +0530

    my-app2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './component/Navbar'
+
+function App() {
+  return (
+    <>
+      {/* <Navbar/> */}
+      <Navbar title="TextUtils"/>
+    </>
+  );
+}
+
+export default App;
+
+
