9fc72819c188be728b2a393a5352f8b3ef8a9efd
ddarshit <darshitdayala007@gmail.com>
2023-08-02 20:02:32 +0530

    my-app2

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Conditionrendring from "./Conditionrendring";
 
 export default function Contact() {
   return (
     <>
       <h1>Contact</h1>
+      <Conditionrendring/>
     </>
   );
 }
