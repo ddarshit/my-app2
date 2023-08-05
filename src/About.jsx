cc9254c5634188af6fd652eb1817194785d917a0
ddarshit <darshitdayala007@gmail.com>
2023-08-05 19:19:44 +0530

    my-app2

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Textform from "./component/Textform";
+import Usestate from "./Usestate";
 
 export default function About() {
   return (
     <>
       <h1>About</h1>
       <Textform/>
+      <Usestate/>
     </>
   );
 }
