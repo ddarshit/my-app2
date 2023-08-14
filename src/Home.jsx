eade4e250bc286eea8c9122e658bb2c81bdaee93
ddarshit <darshitdayala007@gmail.com>
2023-08-14 17:28:22 +0530

    my-app2

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import FormInput from "./FormInput";
-import FormTextarea from "./FormTextarea";
+// import FormInput from "./FormInput";
+// import FormTextarea from "./FormTextarea";
+import TodoList from "./TodoList";
 
 export default function Home() {
   return (
     <>
+    <div className="col-6">
+
       <h1>Home</h1>
-      <FormInput />
-      <FormTextarea />
+      {/* <FormInput />
+      <FormTextarea /> */}
+      <TodoList/>
+      </div>
+      
     </>
   );
 }
