a43682c7100d0cf8e553af3182565153f7c2c5ea
ddarshit <darshitdayala007@gmail.com>
2023-09-01 19:01:10 +0530

    my-app2

---
+++ src/UseEffect.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+function Timer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  }, []); // <- add empty brackets here
+
+  return <h1>I've rendered {count} times!</h1>;
+}
+
+export default Timer;
